90c68725af3fc5356bfef2f0e18c16a335814a69
Yusif Salam-zade <yusif.salam-zade@aalto.fi>
2019-07-03 11:30:43 +0300

    set up cloudinary image upload

--- voimanosto-backend/app.js
+++ voimanosto-backend/app.js
@@ -10,8 +10,32 @@ const exercsiesRouter = require('./controllers/exercises')
 const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+const path = require('path')
+const cloudinary = require('cloudinary')
+const multer = require('multer')
 
-logger.info('connecting to mongodb')
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname)
+  }
+})
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 3
+  },
+  fileFilter: fileFilter
+})
 
 mongoose
   .connect(config.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true })
@@ -21,6 +45,12 @@ mongoose
   .catch(err => {
     console.log('could not connect to MongoDB', err.message)
   })
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+})
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -32,6 +62,20 @@ app.use('/api/users', usersRouter)
 app.use('/api/workouts', workoutsRouter)
 app.use('/api/exercises', exercsiesRouter)
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/build/index.html'))
+})
+
+app.post('/img_upload', upload.single('file'), (req, res, next) => {
+  cloudinary.uploader.upload(req.file.path, function(result, error) {
+    console.log('**uploading file**')
+    console.log('result', result)
+    if (error) {
+      next(error)
+    }
+    res.json(result)
+  })
+})
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
