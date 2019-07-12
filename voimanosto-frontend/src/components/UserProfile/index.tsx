6ec684a5f67f629afe003df8f99544e46a05b419
Yusif Salam-zade <yusif.salam-zade@aalto.fi>
2019-07-12 14:10:34 +0300

    add bodyweight chart with fake data

--- voimanosto-frontend/src/components/UserProfile/index.tsx
+++ voimanosto-frontend/src/components/UserProfile/index.tsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 import { IUser } from '../../types'
+import {
+  AreaChart,
+  Area,
+  Label,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  Tooltip
+} from 'recharts'
+import moment from 'moment'
 
 interface UserProfileProps {
   user?: IUser | null
 }
 
 const UserProfile: React.FC<UserProfileProps> = ({ user }) => {
+  const data = [
+    {
+      bodyweight: 73.5,
+      date: '2019-06-27T10:49:56.350+00:00'
+    },
+    {
+      bodyweight: 80.0,
+      date: '2019-07-01T10:59:08.362+00:00'
+    },
+    {
+      bodyweight: 77.8,
+      date: '2019-07-12T10:59:08.362+00:00'
+    }
+  ]
   return (
-    <div>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       {user !== undefined && user !== null ? (
         <div>
           <Card>
@@ -32,6 +56,24 @@ const UserProfile: React.FC<UserProfileProps> = ({ user }) => {
       ) : (
         <div />
       )}
+      <div style={{ width: '500px', height: '300px', display: 'flex' }}>
+        <ResponsiveContainer>
+          <AreaChart
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 20 }}
+          >
+            <XAxis
+              dataKey='date'
+              tickFormatter={tick => moment(tick).format('MMM DD')}
+            >
+              <Label value='Bodyweight' position='insideBottom' offset={-10} />
+            </XAxis>
+            <YAxis />
+            <Area type='monotone' dataKey='bodyweight' />
+            <Tooltip />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   )
 }
