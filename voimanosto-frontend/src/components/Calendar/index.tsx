6fff40e12ad01007721a0eaed08a9fb858756fd4
Yusif Salam-zade <yusif.salam-zade@aalto.fi>
2019-07-14 20:28:50 +0300

    Fetch real data and improve aesthetics of Calendar

--- voimanosto-frontend/src/components/Calendar/index.tsx
+++ voimanosto-frontend/src/components/Calendar/index.tsx
@@ -1,13 +1,111 @@
-import React from 'react'
-import Calendar, { MonthView } from 'react-calendar'
+import React, { useState } from 'react'
+import Calendar from 'react-calendar'
+import { TransitionablePortal, Button, Form } from 'semantic-ui-react'
+import './CustomCalendar.scss'
+import bodyweightService from '../../services/bodyweightService'
+import moment from 'moment'
+import { IUser } from '../../types'
 
-const CustomCalendar: React.FC = () => {
+interface CalendarProps {
+  user: IUser | null | undefined
+}
+
+const CustomCalendar: React.FC<CalendarProps> = ({ user }) => {
+  const [portalOpen, setPortalOpen] = useState(false)
+  const [popupTopPos, setPopupTopPos] = useState(0)
+  const [popupLeftPos, setPopupLeftPos] = useState(0)
+  const [selectedDay, setSelectedDay] = useState('')
+  const [bodyweight, setBodyweight] = useState(0)
+
+  const handleClick = (event: any) => {
+    setPortalOpen(true)
+    setSelectedDay(event)
+    const btnEl = document.getElementsByClassName(
+      'react-calendar__tile--active'
+    )
+    let btn = btnEl.item(0)
+    const calEl = document.getElementsByClassName(
+      'react-calendar__month-view__days'
+    )
+    let monthBox = calEl.item(0)
+    if (btn && monthBox) {
+      let btnPos = btn.getBoundingClientRect()
+      let boxPos = monthBox.getBoundingClientRect()
+      if (btnPos.top + 200 > boxPos.bottom) setPopupTopPos(boxPos.bottom - 200)
+      else setPopupTopPos(btnPos.top + 15 + btnPos.height / 2)
+      if (btnPos.left + 150 > boxPos.right) setPopupLeftPos(boxPos.right - 150)
+      else setPopupLeftPos(btnPos.left)
+    }
+  }
+
+  const addBodyweight = async () => {
+    const dateObj = moment(selectedDay).toDate()
+    if (user) {
+      const result = await bodyweightService.addBw({
+        date: dateObj,
+        bodyweight: bodyweight,
+        username: user.username,
+        token: user.token
+      })
+      console.log(result)
+    }
+  }
+
   return (
     <div>
       <h1>Calendar y'all</h1>
-      <Calendar />
-      <h1>Month view</h1>
-      <MonthView activeStartDate={new Date(2019, 6, 1)} />
+      <Calendar onChange={handleClick} className='react-calendar' />
+      <TransitionablePortal
+        onClose={() => setPortalOpen(false)}
+        open={portalOpen}
+      >
+        <Button.Group
+          style={{
+            left: `${popupLeftPos}px`,
+            position: 'fixed',
+            top: `${popupTopPos}px`,
+            maxWidth: '150px',
+            maxHeight: '200px',
+            zIndex: 1000,
+            backgroundColor: 'white'
+          }}
+          vertical
+          size='small'
+        >
+          <Button
+            size='mini'
+            color='youtube'
+            style={{ border: '2px white solid' }}
+            onClick={() => addBodyweight()}
+          >
+            Add new bodyweight reading
+          </Button>
+
+          <Button
+            size='mini'
+            color='youtube'
+            style={{ border: '2px white solid' }}
+          >
+            Add new workout
+          </Button>
+
+          <Button
+            size='mini'
+            color='youtube'
+            style={{ border: '2px white solid' }}
+          >
+            Add new competition
+          </Button>
+
+          <div />
+        </Button.Group>
+      </TransitionablePortal>
+      <Form>
+        <Form.Input
+          type='number'
+          onChange={({ target }) => setBodyweight(Number(target.value))}
+        />
+      </Form>
     </div>
   )
 }
