04fc73ad9e1183d9f2904b61eb8291814d87567d
Yusif Salam-zade <yusif.salam-zade@aalto.fi>
2019-07-16 19:17:09 +0300

    make use of verify identity function from utils

--- voimanosto-backend/controllers/bodyweights.js
+++ voimanosto-backend/controllers/bodyweights.js
@@ -5,49 +5,42 @@ const User = require('../models/user')
 const Bodyweight = require('../models/bodyweight')
 
 bodyweightsRouter.post('/', async (req, res, next) => {
-  const body = req.body
-  const token = utils.getTokenFrom(req)
-  try {
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
-      return res.status(401).json({ error: 'token missing or invalid' })
+  const verified = await utils.verifyIdentity(req)
+  if (verified !== true) {
+    res.status(401).json({ error: verified })
+  } else {
+    try {
+      const user = await User.findOne({ username: req.params.username })
+      const newBw = new Bodyweight({
+        date: req.body.date,
+        user: user._id,
+        bodyweight: req.body.bodyweight
+      })
+      const savedBw = await newBw.save()
+      user.bodyweight = user.bodyweight.concat(savedBw._id)
+      await user.save()
+      res.json(savedBw.toJSON())
+    } catch (exception) {
+      next(exception)
     }
-    const user = await User.findOne({ username: req.params.username })
-
-    const newBw = new Bodyweight({
-      date: body.date,
-      user: user._id,
-      bodyweight: body.bodyweight
-    })
-    const savedBw = await newBw.save()
-    user.bodyweight = user.bodyweight.concat(savedBw._id)
-    await user.save()
-    res.json(savedBw.toJSON())
-  } catch (exception) {
-    next(exception)
   }
 })
 
 bodyweightsRouter.get('/', async (req, res, next) => {
-  const token = utils.getTokenFrom(req)
-  try {
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
-      return res.status(401).json({ error: 'token missing or invalid' })
+  const verified = await utils.verifyIdentity(req)
+  if (verified !== true) {
+    res.status(401).json({ error: verified })
+  } else {
+    try {
+      const user = await User.findOne({ username: req.params.username })
+      const bodyweights = await Bodyweight.find({
+        user: user._id
+      }).sort({ date: 'asc' })
+
+      res.json(bodyweights.map(bw => bw.toJSON()))
+    } catch (exception) {
+      next(exception)
     }
-    const user = await User.findOne({ username: req.params.username })
-
-    const bodyweights = await Bodyweight.find({
-      user: user._id
-    })
-      .populate('user', {
-        name: 1
-      })
-      .sort({ date: 'asc' })
-
-    res.json(bodyweights.map(bw => bw.toJSON()))
-  } catch (exception) {
-    next(exception)
   }
 })
 
