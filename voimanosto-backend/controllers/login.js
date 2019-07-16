27adc9216a2deaee4f6b28d1c797ac748c2ca454
Yusif Salam-zade <yusif.salam-zade@aalto.fi>
2019-07-16 19:13:57 +0300

    add API route for token verification

--- voimanosto-backend/controllers/login.js
+++ voimanosto-backend/controllers/login.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
-const Users = require('../models/user')
+const User = require('../models/user')
 
 loginRouter.post('/', async (req, res) => {
   const body = req.body
-  const user = await Users.findOne({ username: body.username })
+  const user = await User.findOne({ username: body.username })
   const passwordCorrect =
     user === null
       ? false
@@ -23,15 +23,28 @@ loginRouter.post('/', async (req, res) => {
   }
   const token = jwt.sign(userForToken, process.env.SECRET)
 
-  res
-    .status(200)
-    .send({
-      token,
-      username: user.username,
-      name: user.name,
-      email: user.email,
-      avatar: user.avatar
-    })
+  res.status(200).send({
+    token,
+    username: user.username,
+    name: user.name,
+    email: user.email,
+    avatar: user.avatar
+  })
+})
+
+loginRouter.post('/verify', async (req, res) => {
+  const token = req.body.token
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  return res.json({
+    username: user.username,
+    name: user.name,
+    email: user.email,
+    avatar: user.avatar
+  })
 })
 
 module.exports = loginRouter
