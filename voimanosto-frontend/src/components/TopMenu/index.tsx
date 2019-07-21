db4369abe1b3312968f7f16aa089b81d46bd3060
Yusif Salam-zade <yusalam@gmail.com>
2019-07-21 23:29:13 +0300

    add notification mockup to top menu

--- voimanosto-frontend/src/components/TopMenu/index.tsx
+++ voimanosto-frontend/src/components/TopMenu/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './TopMenu.scss'
 import { Menu, Image, Button, Icon, Header } from 'semantic-ui-react'
 import useReactRouter from 'use-react-router'
+import { NavLink } from 'react-router-dom'
 
 interface TopMenuProps {
   logo: string
@@ -13,10 +14,17 @@ const TopMenu: React.FC<TopMenuProps> = ({ logo }) => {
     <Menu inverted fixed='top' borderless className='top-menu'>
       <Menu.Item header className='logo'>
         <Image src={logo} size='tiny' />
-        <Header className='headerInLogo'>Voimanosto</Header>
+        <Header className='headerInLogo' as={NavLink} to='/'>
+          Voimanosto
+        </Header>
       </Menu.Item>
       <Menu.Menu position='right'>
         <Menu.Item>
+          <Menu.Item as={NavLink} to='/notifications'>
+            <div className='ui blue basic label'>
+              <i className='ui big icon bell outline ' />2
+            </div>
+          </Menu.Item>
           <Menu.Item>
             <Button
               inverted
