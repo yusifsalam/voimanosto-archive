90ab9f39f30749a6795603da4ec81e4949918d4a
Yusif Salam-zade <yusalam@gmail.com>
2019-07-21 21:24:34 +0300

    add GET route for user stats

---
+++ voimanosto-backend/controllers/stats.js
@@ -0,0 +1,19 @@
+const statsRouter = require('express').Router({ mergeParams: true })
+const utils = require('../utils/middleware')
+const User = require('../models/user')
+const Stats = require('../models/stats')
+
+statsRouter.get('/', async (req, res, next) => {
+  const verified = await utils.verifyIdentity(req)
+  if (verified !== true) {
+    res.status(401).json({ error: verified })
+  } else {
+    try {
+      const user = await User.findOne({ username: req.params.username })
+      const stats = await Stats.findOne({ user: user._id })
+      res.json(stats.toJSON())
+    } catch (exception) {
+      next(exception)
+    }
+  }
+})
