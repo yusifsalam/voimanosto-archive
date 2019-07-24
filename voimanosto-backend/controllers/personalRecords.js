2f35be125c2825681f0cfe52b0fece3afd700bff
Yusif Salam-zade <yusalam@gmail.com>
2019-07-24 13:59:09 +0300

    define API routes for getting PR Table

--- voimanosto-backend/controllers/personalRecords.js
+++ voimanosto-backend/controllers/personalRecords.js
@@ -6,78 +6,139 @@ const PR = require('../models/personalRecord')
 const Exercise = require('../models/exercise')
 
 prRouter.get('/', async (req, res, next) => {
-  const token = utils.getTokenFrom(req)
-  try {
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
-      return res.status(401).json({ error: 'token missing or invalid' })
-    }
+  const verified = await utils.verifyIdentity(req)
+  if (verified !== true) {
+    res.status(401).json({ error: verified })
+  } else {
+    try {
+      const user = await User.findOne({ username: req.params.username })
 
-    const user = await User.findOne({ username: req.params.username })
-    if (decodedToken.id !== user._id.toString()) {
-      return res.status(401).json({ error: 'unauthorized!' })
+      const prs = await PR.aggregate([
+        { $match: { user: user._id } },
+        {
+          $lookup: {
+            from: 'exercises',
+            localField: 'exercise',
+            foreignField: '_id',
+            as: 'exercise'
+          }
+        },
+        { $unwind: '$exercise' },
+        {
+          $sort: {
+            'exercise.type': 1,
+            'exercise.name': 1,
+            'exercise.variation': 1
+          }
+        },
+        {
+          $project: {
+            reps: 1,
+            weight: 1,
+            'exercise.type': 1,
+            'exercise.name': 1,
+            'exercise.variation': 1
+          }
+        }
+      ])
+      res.json(prs)
+    } catch (exception) {
+      next(exception)
     }
-    const prs = await PR.find({
-      user: user._id
-    })
-    res.json(prs.map(pr => pr.toJSON()))
-  } catch (exception) {
-    next(exception)
   }
 })
 
-prRouter.get('/:exercise', async (req, res, next) => {
-  const verified = await utils.verifyIdentity(req, res, next)
-  try {
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
-      return res.status(401).json({ error: 'token missing or invalid' })
-    }
+prRouter.get('/:type', async (req, res, next) => {
+  const verified = await utils.verifyIdentity(req)
+  if (verified !== true) {
+    res.status(401).json({ error: verified })
+  } else {
+    try {
+      const user = await User.findOne({ username: req.params.username })
 
-    const user = await User.findOne({ username: req.params.username })
-    if (decodedToken.id !== user._id.toString()) {
-      return res.status(401).json({ error: 'unauthorized!' })
+      const prs = await PR.aggregate([
+        { $match: { user: user._id } },
+        {
+          $lookup: {
+            from: 'exercises',
+            localField: 'exercise',
+            foreignField: '_id',
+            as: 'exercise'
+          }
+        },
+        { $unwind: '$exercise' },
+        { $match: { 'exercise.type': req.params.type } },
+        {
+          $sort: {
+            'exercise.type': 1,
+            'exercise.name': 1,
+            'exercise.variation': 1,
+            reps: 1
+          }
+        },
+        {
+          $project: {
+            reps: 1,
+            weight: 1,
+            'exercise.type': 1,
+            'exercise.name': 1,
+            'exercise.variation': 1
+          }
+        }
+      ])
+      res.json(prs)
+    } catch (exception) {
+      next(exception)
     }
-    const exercise = await Exercise.findOne({ name: req.params.exercise })
-    if (!exercise) {
-      return res.status(404).json({ error: 'exercise not found' })
-    }
-    const prs = await PR.find({
-      user: user._id,
-      exercise: exercise.name
-    })
-    res.json(prs.map(pr => pr.toJSON()))
-  } catch (exception) {
-    next(exception)
   }
 })
 
-prRouter.get('/:exercise/:variation', async (req, res, next) => {
-  const token = utils.getTokenFrom(req)
-  try {
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
-      return res.status(401).json({ error: 'token missing or invalid' })
-    }
+prRouter.get('/:type/:name', async (req, res, next) => {
+  const verified = await utils.verifyIdentity(req)
+  if (verified !== true) {
+    res.status(401).json({ error: verified })
+  } else {
+    try {
+      const user = await User.findOne({ username: req.params.username })
 
-    const user = await User.findOne({ username: req.params.username })
-    if (decodedToken.id !== user._id.toString()) {
-      return res.status(401).json({ error: 'unauthorized!' })
+      const prs = await PR.aggregate([
+        { $match: { user: user._id } },
+        {
+          $lookup: {
+            from: 'exercises',
+            localField: 'exercise',
+            foreignField: '_id',
+            as: 'exercise'
+          }
+        },
+        { $unwind: '$exercise' },
+        {
+          $match: {
+            'exercise.type': req.params.type,
+            'exercise.name': req.params.name
+          }
+        },
+        {
+          $sort: {
+            'exercise.type': 1,
+            'exercise.name': 1,
+            'exercise.variation': 1
+          }
+        },
+        {
+          $project: {
+            reps: 1,
+            weight: 1,
+            'exercise.type': 1,
+            'exercise.name': 1,
+            'exercise.variation': 1
+          }
+        }
+      ])
+      res.json(prs)
+    } catch (exception) {
+      next(exception)
     }
-    const exercise = await Exercise.findOne({
-      name: req.params.exercise,
-      variation: req.params.variation
-    })
-    if (!exercise) {
-      return res.status(404).json({ error: 'exercise not found' })
-    }
-    const prs = await PR.find({
-      user: user._id,
-      exercise: exercise.name
-    })
-    res.json(prs.map(pr => pr.toJSON()))
-  } catch (exception) {
-    next(exception)
   }
 })
 
