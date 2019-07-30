d834b10f7dd5abacf2826ce75c99afda38356ab3
Yusif Salam-zade <yusalam@gmail.com>
2019-07-30 10:57:55 +0300

    Dashboard now shows today's or current week's workouts

--- voimanosto-frontend/src/views/Dashboard/Dashboard.tsx
+++ voimanosto-frontend/src/views/Dashboard/Dashboard.tsx
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import moment from 'moment'
 import { UserContext } from '../../context/userContext'
-import { Header } from 'semantic-ui-react'
+import { Header, Checkbox } from 'semantic-ui-react'
+import Workout from '../../components/Workouts/Workout'
+import Workouts from '../../components/Workouts'
 
 const Dashboard: React.FC = () => {
   const { user } = useContext(UserContext)
+  const [checked, setChecked] = useState(false)
   return (
     <div>
       <Header inverted as='h3'>
@@ -13,6 +16,21 @@ const Dashboard: React.FC = () => {
       <Header inverted as='h5'>
         Today is {moment().format('MMMM DD, YYYY')}
       </Header>
+      {/* <Workout date={moment().toDate()} exercises={[]} /> */}
+      <Header inverted as='h5'>
+        Daily
+        <Checkbox
+          slider
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+        />
+        Weekly
+      </Header>
+      {!checked ? (
+        <Workout date={moment().toDate()} exercises={[]} />
+      ) : (
+        <Workouts />
+      )}
     </div>
   )
 }
