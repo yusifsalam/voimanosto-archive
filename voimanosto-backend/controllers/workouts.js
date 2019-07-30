dad74a478a4ba45191dc18b9d3adbe172b6b9a98
Yusif Salam-zade <yusalam@gmail.com>
2019-07-30 11:08:44 +0300

    define GET routes for workout controller

--- voimanosto-backend/controllers/workouts.js
+++ voimanosto-backend/controllers/workouts.js
@@ -1,8 +1,7 @@
 const workoutsRouter = require('express').Router({ mergeParams: true })
 const Workout = require('../models/workout')
 const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-const getTokenFrom = require('../utils/middleware')
+const utils = require('../utils/middleware')
 
 workoutsRouter.post('/', async (req, res, next) => {
   const body = req.body
@@ -30,22 +29,35 @@ workoutsRouter.post('/', async (req, res, next) => {
 })
 
 workoutsRouter.get('/', async (req, res) => {
-  const user = await User.findOne({ username: req.params.username })
-  const workouts = await Workout.find({
-    user: user._id
-  }).populate('user', {
-    username: 1,
-    name: 1
-  })
-  res.json(workouts.map(workout => workout.toJSON()))
+  const verified = await utils.verifyIdentity(req)
+  if (verified !== true) {
+    res.status(401).json({ error: verified })
+  } else {
+    try {
+      const user = await User.findOne({ username: req.params.username })
+      const workouts = await Workout.find({ user: user._id })
+      res.json(workouts.map(workout => workout.toJSON()))
+    } catch (exception) {
+      next(exception)
+    }
+  }
 })
 
 workoutsRouter.get('/:date', async (req, res, next) => {
-  try {
-    const workout = await Workout.findOne({ date: req.params.date })
-    res.json(workout.toJSON())
-  } catch (exception) {
-    next(exception)
+  const verified = await utils.verifyIdentity(req)
+  if (verified !== true) {
+    res.status(401).json({ error: verified })
+  } else {
+    try {
+      const user = await User.findOne({ username: req.params.username })
+      const workout = await Workout.findOne({
+        date: req.params.date,
+        user: user._id
+      })
+      res.json(workout.toJSON())
+    } catch (exception) {
+      next(exception)
+    }
   }
 })
 
