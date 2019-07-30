f0d2bdc7bed1ee775f7185803b409807099d5541
Yusif Salam-zade <yusalam@gmail.com>
2019-07-30 22:56:45 +0300

    add GET route for fetching 1RM of an exercise

--- voimanosto-backend/controllers/personalRecords.js
+++ voimanosto-backend/controllers/personalRecords.js
@@ -2,6 +2,7 @@ const prRouter = require('express').Router({ mergeParams: true })
 const utils = require('../utils/middleware')
 const User = require('../models/user')
 const PR = require('../models/personalRecord')
+const Exercise = require('../models/exercise')
 
 prRouter.get('/:current', async (req, res, next) => {
   const verified = await utils.verifyIdentity(req)
@@ -247,6 +248,31 @@ prRouter.get('/:type/:name/:current', async (req, res, next) => {
         ])
       }
       res.json(prs)
+    } catch (exception) {
+      next(exception)
+    }
+  }
+})
+
+prRouter.get('/:type/:name/:variation/:current', async (req, res, next) => {
+  const verified = await utils.verifyIdentity(req)
+  if (verified !== true) {
+    res.status(401).json({ error: verified })
+  } else {
+    try {
+      const user = await User.findOne({ username: req.params.username })
+      const exercise = await Exercise.findOne({
+        type: req.params.type,
+        name: req.params.name,
+        variation: req.params.variation,
+        user: user._id
+      })
+      const pr = await PR.findOne({
+        exercise: exercise._id,
+        reps: 1,
+        isCurrentPR: true
+      })
+      res.json(pr.toJSON())
     } catch (exception) {
       next(exception)
     }
