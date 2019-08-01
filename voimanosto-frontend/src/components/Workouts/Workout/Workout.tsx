8ac31785f6517502b9affbf300bf38565a9b1bf9
Yusif Salam-zade <yusalam@gmail.com>
2019-08-01 17:35:57 +0300

    add colors to the workout view

--- voimanosto-frontend/src/components/Workouts/Workout/Workout.tsx
+++ voimanosto-frontend/src/components/Workouts/Workout/Workout.tsx
@@ -36,7 +36,12 @@ const Workout: React.FC<WorkoutProps> = ({ date }) => {
   return (
     <div>
       <Header inverted>{moment(date).format('MMMM Do')}</Header>
-      <Accordion styled inverted exclusive={false}>
+      <Accordion
+        styled
+        inverted
+        exclusive={false}
+        style={{ backgroundColor: '#1C1C1E' }}
+      >
         {!exercises ? (
           <p>No workout </p>
         ) : (
@@ -44,33 +49,49 @@ const Workout: React.FC<WorkoutProps> = ({ date }) => {
             <div key={i}>
               <Accordion.Title
                 as={Header}
-                // inverted
                 index={i}
                 active={activeExerciseIndex.includes(i)}
                 key={ex.id}
                 onClick={() => handleClick(i)}
+                style={{
+                  marginTop: '10px',
+                  color: 'white',
+                  borderRadius: '10px',
+                  backgroundColor:
+                    ex.exercise.name === 'Squat'
+                      ? '#ff375f'
+                      : ex.exercise.name === 'Bench'
+                      ? '#ff9f0a'
+                      : ex.exercise.name === 'Deadlift'
+                      ? '#5e5ce6'
+                      : '#2c2c2e'
+                }}
               >
                 <Icon name='dropdown' />
                 {ex.exercise.name} ({ex.exercise.variation})
               </Accordion.Title>
               <Accordion.Content active={activeExerciseIndex.includes(i)}>
-                <Table inverted>
+                <Table inverted unstackable collapsing>
                   <Table.Header>
                     <Table.Row>
-                      <Table.HeaderCell>Exercise</Table.HeaderCell>
                       <Table.HeaderCell>Sets</Table.HeaderCell>
                       <Table.HeaderCell>Reps</Table.HeaderCell>
                       <Table.HeaderCell>Weight</Table.HeaderCell>
                       <Table.HeaderCell>Intensity</Table.HeaderCell>
+                      <Table.HeaderCell>RPE</Table.HeaderCell>
+                      <Table.HeaderCell>PR</Table.HeaderCell>
                     </Table.Row>
                   </Table.Header>
                   <Table.Body>
                     <Table.Row>
-                      <Table.Cell>{ex.exercise.name}</Table.Cell>
                       <Table.Cell>{ex.sets}</Table.Cell>
                       <Table.Cell>{ex.reps}</Table.Cell>
                       <Table.Cell>{ex.weight}</Table.Cell>
                       <Table.Cell>{ex.intensity}</Table.Cell>
+                      <Table.Cell>{ex.RPE}</Table.Cell>
+                      <Table.Cell>
+                        {ex.isPR ? <Icon name='check' /> : <div />}
+                      </Table.Cell>
                     </Table.Row>
                   </Table.Body>
                 </Table>
