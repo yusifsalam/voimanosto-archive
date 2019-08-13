044737999c6b556fa60d178fef9980d477f5f816
Yusif Salam-zade <yusalam@gmail.com>
2019-08-13 15:03:28 +0300

    add registration form validation with Formik and yup

--- voimanosto-frontend/src/components/RegistrationForm/RegistrationForm.tsx
+++ voimanosto-frontend/src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,163 +1,145 @@
-import React, { useState } from 'react'
-import { Form, Button, Message } from 'semantic-ui-react'
-import EmailValidator from 'email-validator'
-import registrationService from '../../services/registrationService'
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+import { FormikErrors, FormikProps, withFormik } from 'formik'
+import React from 'react'
+import { Button, Form, Message } from 'semantic-ui-react'
+import registrationService from 'services/registrationService'
+import * as yup from 'yup'
 
-const RegistrationForm: React.FC = () => {
-  const [name, setName] = useState('')
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState<string | null>(null)
-  const [successMessage, setSuccessMessage] = useState<string | null>(null)
-  const [nameError, setNameError] = useState<string | boolean>(false)
-  const [usernameError, setUsernameError] = useState<string | boolean>(false)
-  const [emailError, setEmailError] = useState<string | boolean>(false)
-  const [passwordError, setPasswordError] = useState<string | boolean>(false)
-  const [confirmPasswordError, setConfirmPasswordError] = useState<
-    string | boolean
-  >(false)
-
-  const emptyFields = () => {
-    setName('')
-    setUsername('')
-    setEmail('')
-    setPassword('')
-    setConfirmPassword('')
-  }
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    if (nameError || usernameError || emailError)
-      setErrorMessage('Invalid field values present!')
-    else if (password !== confirmPassword)
-      setErrorMessage('Passwords do not match')
-    else {
-      try {
-        const user = await registrationService.register({
-          username,
-          name,
-          email,
-          password
-        })
-        emptyFields()
-
-        setSuccessMessage(`Registration successful for ${user.name}`)
-      } catch (exception) {
-        setErrorMessage(exception.response.data.error)
-      }
-    }
-    setTimeout(() => {
-      setSuccessMessage(null)
-      setErrorMessage(null)
-    }, 3000)
-  }
-
-  const validateName = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    let newName = event.currentTarget.value
-    if (newName.length < 3) setNameError('Name too short')
-    else setNameError(false)
-  }
-
-  const validateUsername = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    if (username.length < 3) setUsernameError('Username too short')
-    else setUsernameError(false)
-  }
-
-  const validateEmail = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    let validEmail = EmailValidator.validate(email)
-    if (!validEmail) setEmailError('Invalid email address')
-    else setEmailError(false)
-  }
-
-  const validatePassword = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    if (password === '') setPasswordError('Password cannot be empty')
-    else if (password.length < 5)
-      setPasswordError('Password must contain at least 5 symbols')
-    else setPasswordError(false)
-  }
-  const validateConfirmPassword = (
-    event: React.FormEvent<HTMLInputElement>
-  ) => {
-    event.preventDefault()
-    if (confirmPassword === '')
-      setConfirmPasswordError('Password cannot be empty')
-    else if (confirmPassword.length < 5)
-      setConfirmPasswordError('Password must contain at least 5 symbols')
-    else setConfirmPasswordError(false)
-  }
-
-  return (
-    <div>
-      {errorMessage === null ? (
-        <div />
-      ) : (
-        <Message negative>{errorMessage}</Message>
-      )}
-      {successMessage === null ? (
-        <div />
-      ) : (
-        <Message positive>{successMessage}</Message>
-      )}
-      <Form inverted onSubmit={handleSubmit}>
-        <Form.Input
-          error={nameError}
-          icon='user'
-          iconPosition='left'
-          label='Name'
-          placeholder='Name'
-          value={name}
-          onChange={({ target }) => setName(target.value)}
-          onBlur={validateName}
-        />
-        <Form.Input
-          error={usernameError}
-          icon='user plus'
-          iconPosition='left'
-          label='Username'
-          placeholder='Username'
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-          onBlur={validateUsername}
-        />
-        <Form.Input
-          error={emailError}
-          icon='envelope'
-          iconPosition='left'
-          label='Email'
-          placeholder='Email'
-          value={email}
-          onChange={({ target }) => setEmail(target.value)}
-          onBlur={validateEmail}
-        />
-        <Form.Input
-          error={passwordError}
-          icon='lock'
-          iconPosition='left'
-          label='Password'
-          type='password'
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-          onBlur={validatePassword}
-        />
-        <Form.Input
-          error={confirmPasswordError}
-          icon='lock'
-          iconPosition='left'
-          label='Confirm Password'
-          type='password'
-          value={confirmPassword}
-          onChange={({ target }) => setConfirmPassword(target.value)}
-          onBlur={validateConfirmPassword}
-        />
-        <Button type='submit'>Register</Button>
-      </Form>
-    </div>
-  )
+interface FormValues {
+  name: string
+  username: string
+  email: string
+  password: string
+  confirmPassword: string
 }
 
-export default RegistrationForm
+interface Props {
+  submit?: (values: FormValues) => Promise<FormikErrors<FormValues> | null>
+}
+
+const RegistrationSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(3, 'Name too short!')
+    .max(25, 'Name too long!')
+    .required('Name is a required field!'),
+  username: yup
+    .string()
+    .min(3, 'Username too short')
+    .max(10, 'Username too long!')
+    .required('Username is a required field'),
+  email: yup
+    .string()
+    .email('Invalid email')
+    .required('Email is required!'),
+  password: yup
+    .string()
+    .min(5, 'Password too short')
+    .max(25, 'Password too long')
+    .required('Password missing'),
+  confirmPassword: yup
+    .string()
+    .min(5, 'Too short')
+    .max(25, 'Too long')
+    .test('passwords-match', 'Passwords must match', function(value) {
+      return this.parent.password === value
+    })
+    .required('Please confirm the password'),
+})
+
+class RegistrationForm extends React.PureComponent<Props & FormikProps<FormValues>> {
+  render(): React.ReactNode {
+    const { status, values, handleChange, handleBlur, touched, errors, handleSubmit } = this.props
+    return (
+      <div>
+        <Message error content={status && status.msg} hidden={!status} />
+        <Form inverted onSubmit={handleSubmit}>
+          <Form.Input
+            name="name"
+            icon="user"
+            iconPosition="left"
+            label="Name"
+            placeholder="Name"
+            error={touched.name && errors.name}
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Form.Input
+            name="username"
+            icon="user plus"
+            iconPosition="left"
+            label="Username"
+            placeholder="Username"
+            error={touched.username && errors.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Form.Input
+            name="email"
+            icon="envelope"
+            iconPosition="left"
+            label="Email"
+            placeholder="Email"
+            error={touched.email && errors.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Form.Input
+            name="password"
+            icon="lock"
+            iconPosition="left"
+            label="Password"
+            type="password"
+            error={touched.password && errors.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Form.Input
+            name="confirmPassword"
+            icon="lock"
+            iconPosition="left"
+            label="Confirm Password"
+            type="password"
+            error={touched.confirmPassword && errors.confirmPassword}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Button type="submit">Register</Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+interface MyFormProps {
+  initialEmail?: string
+}
+
+const ValidatedForm = withFormik<MyFormProps, FormValues>({
+  validationSchema: RegistrationSchema,
+  mapPropsToValues: () => ({
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  }),
+  handleSubmit: async (values, { setStatus, resetForm }) => {
+    try {
+      const user = await registrationService.register(values)
+      if (user.error) {
+        setStatus(user.error)
+      } else {
+        resetForm()
+      }
+    } catch (exception) {
+      setStatus({ msg: 'Registration failed! Perhaps the username is taken' })
+      setTimeout(() => {
+        setStatus(undefined)
+      }, 3000)
+    }
+  },
+})(RegistrationForm)
+
+export default ValidatedForm
